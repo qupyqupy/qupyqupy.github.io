468253d7c4946baf282e350e4337003813246b86
stoner <stoner@gmail.com>
2023-10-04 15:22:41 +0800

    天地劫 更新魔堡拉人小程式

--- laren/index.f6a9d.js
+++ laren/index.f6a9d.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.fcd16.js"], function (_export, _context) {
   "use strict";
 
   var createApplication;
@@ -67,8 +67,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      createApplication = _applicationJs.createApplication;
+    setters: [function (_applicationFcd16Js) {
+      createApplication = _applicationFcd16Js.createApplication;
     }],
     execute: function () {
       createApplication({
